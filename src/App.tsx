8711fbe860d2547920201b608fffe82e3b5bb931
m-reinisch <michael.reinisch@email.de>
2026-06-11 12:05:13 +0200

    React Map

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,32 @@ import Article from "./Article.tsx";
 import type {BlogPostProps} from "./BlogPostProps.tsx";
 
 function App() {
-    const blog: BlogPostProps= {
+    const blog1: BlogPostProps= {
+        titel: "Wo es Apfelwein in Frankfurt gibt",
+        subtitel: "Locations",
+        image: "src/assets/Apfelwein_Geripptes_Bembel.jpg",
+        author: "Michael R"
+    };
+    const blog2: BlogPostProps= {
         titel: "Bester Ebbelwoi in Frankfurt",
         subtitel: "Ebbelwoi-Locations",
         image: "src/assets/Apfelwein_Geripptes_Bembel.jpg",
         author: "Michael R"
-    }
+    };
+    const blog3: BlogPostProps= {
+        titel: "Frankfurt the city of apple wine",
+        subtitel: "The best locations",
+        image: "src/assets/Apfelwein_Geripptes_Bembel.jpg",
+        author: "Michael R"
+    };
+    const blogs: BlogPostProps[]= [blog1, blog2, blog3];
 
     return (
     <>
         <Header />
         <main>
-            <Article articleEntry={blog} />
+            {blogs.map(
+                b => <Article articleEntry={b} key={b.titel} />)}
         </main>
         <Footer />
     </>
