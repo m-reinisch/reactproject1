c16c9a0736c9e59176c64470a3de7d31e05eee9d
m-reinisch <michael.reinisch@email.de>
2026-06-16 10:10:07 +0200

    step 1

--- src/Likes.tsx
+++ src/Likes.tsx
@@ -2,14 +2,27 @@ import {useState} from "react";
 
 export default function Likes() {
     const [count, setCount]= useState<number>(0);
+    function changeCounter(like: boolean){
+        if (like === true){
+            setCount( count + 1 )
+        } else {
+            if (count > 0){
+                setCount( count - 1 )
+            }
+        }
+    }
 
     return(
         <>
             <p>Likes: {count}</p>
             <button type="button" onClick={
-                ()=> setCount( count + 1 )}>👍</button>
+                ()=> {
+                    changeCounter(true)
+                    console.log(count)
+                }
+            }>👍</button>
             <button type="button" onClick={
-                ()=> setCount( count - 1 )}>👎</button>
+                ()=> changeCounter(false)}>👎</button>
         </>
     )
 }
